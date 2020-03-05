cd81e433cb473ecdbbf020b775725dd71a156a65
chris <chr33s@icloud.com>
2020-03-05 13:50:22 +0700

    update: packages

--- test.js
+++ test.js
@@ -1,77 +1,77 @@
 /* eslint-env mocha */
 
-'use strict';
+"use strict";
 
-const supertest = require('supertest');
-const assert = require('assert');
-const micro = require('./');
+const supertest = require("supertest");
+const assert = require("assert");
+const micro = require("./");
 
-describe('micro', function() {
-  it('listen', () => {
-    assert('listen' in micro());
-    assert(typeof micro().listen === 'function');
+describe("micro", function() {
+  it("listen", () => {
+    assert("listen" in micro());
+    assert(typeof micro().listen === "function");
   });
 
-  describe('error', function() {
-    it('statusCode', () => {
+  describe("error", function() {
+    it("statusCode", () => {
       try {
         micro.error({ statusCode: 404 });
       } catch (e) {
-        assert(e.statusMessage === 'Not Found');
+        assert(e.statusMessage === "Not Found");
       }
     });
 
-    describe('statusMessage', function() {
-      it('!expose', () => {
+    describe("statusMessage", function() {
+      it("!expose", () => {
         try {
-          micro.error({ statusMessage: 'test' });
+          micro.error({ statusMessage: "test" });
         } catch (e) {
-          assert(e.statusMessage !== 'test');
+          assert(e.statusMessage !== "test");
         }
       });
 
-      it('expose', () => {
+      it("expose", () => {
         try {
-          micro.error({ statusMessage: 'test', expose: true });
+          micro.error({ statusMessage: "test", expose: true });
         } catch (e) {
-          assert(e.statusMessage === 'test');
+          assert(e.statusMessage === "test");
         }
       });
     });
 
-    it('args', () => {
+    it("args", () => {
       try {
-        micro.error({ extra: 'extra attribute' });
+        micro.error({ extra: "extra attribute" });
       } catch (e) {
-        assert(e.extra === 'extra attribute');
+        assert(e.extra === "extra attribute");
       }
     });
 
-    it('default', () => {
+    it("default", () => {
       const app = micro((req, res) => {
         return Promise.reject(new Error());
       });
 
       return supertest(app)
-        .get('/')
+        .get("/")
         .expect(500);
     });
   });
 
-  describe('parse', function() {
-    describe('buffer', function() {
-      it('read', () => {
+  describe("parse", function() {
+    describe("buffer", function() {
+      it("read", () => {
         const app = micro(req => micro.buffer(req));
 
         return supertest(app)
-          .post('/')
-          .send(Buffer.from('buffer'))
+          .post("/")
+          .send(Buffer.from("buffer"))
           .expect(200)
-          .expect('Content-Type', /octet-stream/)
+          .expect("Content-Type", /octet-stream/)
           .then(res => assert(Buffer.isBuffer(res.body)));
       });
 
-      it('rereadable', () => {
+      it("rereadable", () => {
         const app = micro(async req => {
           const b1 = await micro.buffer(req);
           const b2 = await micro.buffer(req);
@@ -82,97 +82,97 @@ describe('micro', function() {
         });
 
         return supertest(app)
-          .post('/')
-          .send('buffer')
+          .post("/")
+          .send("buffer")
           .expect(200);
       });
     });
 
-    it('text', () => {
+    it("text", () => {
       const app = micro(req => micro.text(req));
 
       return supertest(app)
-        .post('/')
-        .send('text')
+        .post("/")
+        .send("text")
         .expect(200)
-        .expect('Content-Type', /plain/)
-        .then(res => assert(res.text === 'text'));
+        .expect("Content-Type", /plain/)
+        .then(res => assert(res.text === "text"));
     });
 
-    it('json', () => {
+    it("json", () => {
       const app = micro(req => micro.json(req));
-      const body = { a: 'b' };
+      const body = { a: "b" };
 
       return supertest(app)
-        .post('/')
+        .post("/")
         .send(body)
         .expect(200)
-        .expect('Content-Type', /json/)
+        .expect("Content-Type", /json/)
         .then(res => assert.deepEqual(res.body, body));
     });
 
-    it('form', () => {
+    it("form", () => {
       const app = micro(req => micro.form(req));
 
       return supertest(app)
-        .post('/')
-        .send('a=b')
+        .post("/")
+        .send("a=b")
         .expect(200)
-        .expect('Content-Type', /json/)
-        .then(res => assert.deepEqual(res.body, { a: 'b' }));
+        .expect("Content-Type", /json/)
+        .then(res => assert.deepEqual(res.body, { a: "b" }));
     });
 
-    it('error', () => {
+    it("error", () => {
       const app = micro(req => micro.json(req));
       const body = '{ "a:b" }';
 
       return supertest(app)
-        .post('/')
+        .post("/")
         .send(body)
         .expect(400)
-        .expect('Content-Type', /plain/);
+        .expect("Content-Type", /plain/);
     });
   });
 
-  describe('response', function() {
-    it('default', () => {
+  describe("response", function() {
+    it("default", () => {
       const app = micro((req, res) => {
-        res.setHeader('Content-Type', 'text/html');
+        res.setHeader("Content-Type", "text/html");
 
-        return '';
+        return "";
       });
 
       return supertest(app)
-        .get('/')
+        .get("/")
         .expect(200)
-        .expect('Content-Type', /html/);
+        .expect("Content-Type", /html/);
     });
 
-    it('null', () => {
+    it("null", () => {
       const app = micro(() => null);
 
       return supertest(app)
-        .get('/')
+        .get("/")
         .expect(204)
-        .then(res => assert(res.text === ''));
+        .then(res => assert(res.text === ""));
     });
 
-    it('nok', () => {
+    it("nok", () => {
       const app = micro((req, res) => {
         res.statusCode = 404;
-        return 'Error';
+        return "Error";
       });
 
       return supertest(app)
-        .get('/')
+        .get("/")
         .expect(404);
     });
 
-    it('ok', () => {
-      const app = micro(() => 'text');
+    it("ok", () => {
+      const app = micro(() => "text");
 
       return supertest(app)
-        .get('/')
+        .get("/")
         .expect(200);
     });
   });
